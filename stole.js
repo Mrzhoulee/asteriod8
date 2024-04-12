829c1585667500ffe2cef9b07ad1cd0ad6b28e1e
Otto Tan <123855692+Mrzhoulee@users.noreply.github.com>
2024-04-12 16:15:06 +0700

    Update stole.js

--- stole.js
+++ stole.js
@@ -4,4 +4,10 @@ let width = canvas.width;
 let height = canvas.height;
 ctx.fillStyle = "black";
 ctx.fillRect(0,0, width, height);
+
+const BALL_SIZE = 5;
+let ballPosition = { x: 20, y: 30};
+
+ctx.fillStyle = "white";
+ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE);
   
