136398ec9733b39a5b309c9738e0978ea30d14f7
Otto Tan <123855692+Mrzhoulee@users.noreply.github.com>
2024-04-21 12:57:14 +0700

    Update stole.js

--- stole.js
+++ stole.js
@@ -8,6 +8,45 @@ ctx.fillRect(0,0, width, height);
 const BALL_SIZE = 5;
 let ballPosition = { x: 20, y: 30};
 
+let xSpeed = 4;
+let ySpeed = 2;
+
+
+
+function draw(){  
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, width, height,);
+
 ctx.fillStyle = "white";
 ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE);
-  
+}
+
+function update() {
+  ballPosition.x += xSpeed;
+  ballPosition.y += ySpeed;
+}
+
+function checkCollision() {
+ let ball = {
+   left: ballPosition.x,
+   right: ballPosition.x + BALL_SIZE,
+   top: ballPosition.y,
+   bottom: ballPosition.y + BALL_SIZE,
+ }
+  if (ball.left < 0 || ball.right > width) {
+    xSpeed = -xSpeed;
+  }
+  if (ball.top < 0 || ball.bottom > height) {
+    ySpeed = -ySpeed;
+  }
+}
+
+function gameLoop() {
+  draw();
+  update();
+  checkCollision();
+  //Call this function again after a timeout
+  setTimeout(gameLoop, 30);
+}
+
+gameLoop();
