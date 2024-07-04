d3825c34cb8e5f82beba8b12a340b1a61c589c07
Otto Tan <123855692+Mrzhoulee@users.noreply.github.com>
2024-07-04 16:39:42 +0700

    Update stole.js

--- stole.js
+++ stole.js
@@ -1,75 +1,99 @@
-let canvas = document.querySelector("#canvas");
-let ctx = canvas.getContext("2d");
-let width = canvas.width;
-let height = canvas.height;
-ctx.fillStyle = "black";
-ctx.fillRect(0,0, width, height);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Canvas Game</title>
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      background-color: black;
+    }
+    canvas {
+      border: 1px solid white;
+    }
+  </style>
+</head>
+<body>
+  <canvas id="canvas" width="400" height="300"></canvas>
+  
+  <script>
+    const canvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext("2d");
+    const width = canvas.width;
+    const height = canvas.height;
 
-const BALL_SIZE = 5;
-let ballPosition = { x: 20, y: 30};
+    const BALL_SIZE = 5;
+    let ballPosition = { x: 20, y: 30 };
 
-let xSpeed = 4;
-let ySpeed = 2;
+    let xSpeed = 4;
+    let ySpeed = 2;
 
-const PADDLE_WIDTH = 5;
-const PADDLE_HEIGHT = 20;
-const PADDLE_OFFSET = 10;
+    const PADDLE_WIDTH = 5;
+    const PADDLE_HEIGHT = 20;
+    const PADDLE_OFFSET = 10;
 
-let leftPaddleTop = 10;
-let rightPaddleTop = 30;
+    let leftPaddleTop = 10;
+    let rightPaddleTop = 30;
 
-document.addEventListener("mousemove",  e => { 
-  rightPaddleTop = e.y - canvas.offsetTop;
-});
+    document.addEventListener("mousemove", e => { 
+      rightPaddleTop = e.clientY - canvas.offsetTop - PADDLE_HEIGHT / 2;
+    });
 
-function draw(){  
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, width, height,);
+    function draw() {
+      ctx.clearRect(0, 0, width, height); // Clear canvas
+      
+      ctx.fillStyle = "white";
+      ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE);
 
-ctx.fillStyle = "white";
-ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE);
-}
+      ctx.fillRect(
+        PADDLE_OFFSET,
+        leftPaddleTop,
+        PADDLE_WIDTH,
+        PADDLE_HEIGHT
+      );
 
-ctx.fillRect(
-  PADDLE_OFFSET,
-  leftPaddleTop,
-  PADDLE_WIDTH,
-  PADDLE_HEIGHT
-  );
-ctx.fillRect(
-  width - PADDLE_WIDTH - PADDLE_OFFSET,
-  rightPaddleTop,
-  PADDLE_WIDTH,
-  PADDLE_HEIGHT
-  );
-} 
+      ctx.fillRect(
+        width - PADDLE_WIDTH - PADDLE_OFFSET,
+        rightPaddleTop,
+        PADDLE_WIDTH,
+        PADDLE_HEIGHT
+      );
+    }
 
-function update() {
-  ballPosition.x += xSpeed;
-  ballPosition.y += ySpeed;
-}
+    function update() {
+      ballPosition.x += xSpeed;
+      ballPosition.y += ySpeed;
+    }
 
-function checkCollision() {
- let ball = {
-   left: ballPosition.x,
-   right: ballPosition.x + BALL_SIZE,
-   top: ballPosition.y,
-   bottom: ballPosition.y + BALL_SIZE,
- }
-  if (ball.left < 0 || ball.right > width) {
-    xSpeed = -xSpeed;
-  }
-  if (ball.top < 0 || ball.bottom > height) {
-    ySpeed = -ySpeed;
-  }
-}
+    function checkCollision() {
+      let ball = {
+        left: ballPosition.x,
+        right: ballPosition.x + BALL_SIZE,
+        top: ballPosition.y,
+        bottom: ballPosition.y + BALL_SIZE,
+      };
 
-function gameLoop() {
-  draw();
-  update();
-  checkCollision();
-  //Call this function again after a timeout
-  setTimeout(gameLoop, 30);
-}
+      if (ball.left < 0 || ball.right > width) {
+        xSpeed = -xSpeed;
+      }
+      if (ball.top < 0 || ball.bottom > height) {
+        ySpeed = -ySpeed;
+      }
+    }
 
-gameLoop();
+    function gameLoop() {
+      draw();
+      update();
+      checkCollision();
+      requestAnimationFrame(gameLoop); // Use requestAnimationFrame for smoother animation
+    }
+
+    gameLoop();
+  </script>
+</body>
+</html>
